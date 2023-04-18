6ce2f3247edff4e48a1aff5892fdf262e4b837fa
NULL <NULL>
2023-04-17 21:21:09 -0400

    alert service

---
+++ my-app/src/js/AlertSvc.js
@@ -0,0 +1,38 @@
+import {Collapse, Alert, AlertTitle} from "@mui/material";
+
+function simpleAlert (severity, message) {
+    return(`<Alert severity=${severity}>${message}</Alert>`);
+}
+
+function normalAlert (severity, title, message) {
+    return(
+        `<Alert severity=${severity}>
+            <AlertTitle>${title}</AlertTitle>
+            ${message}
+            </Alert>
+    `);
+}
+
+const [open, setOpen] = React.useState(true);
+
+function closableAlert (message) {
+    return `<Collapse in={open}><Alert action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}>
+          ${message}
+        </Alert>
+        </Collapse>>`
+}
+
+
+export { simpleAlert, normalAlert, closableAlert}
