09bba9e387ee41ecc713d101f3f4fcffa452d0fd
Albert Luo <aj.luo@mail.utoronto.ca>
2023-04-18 14:47:22 -0400

    made progress on navbar and dashboard stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,25 @@ import React from "react";
 import "./App.css";
 import SignUpForm from "./components/signup_form";
 import SignInForm from "./components/signin_form";
-import Navbar from "./components/navbar";
+import Dashboard from "./components/dashboard";
+import Host_Dashboard from "./components/host_dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
           <Route path="/" element={<SignUpForm />}>
             {" "}
           </Route>
           <Route path="/signIn" element={<SignInForm />}>
+            {" "}
+          </Route>
+          <Route path="/dashboard" element={<Dashboard />}>
+            {" "}
+          </Route>
+          <Route path="/host_dashboard" element={<Host_Dashboard />}>
             {" "}
           </Route>
         </Routes>
